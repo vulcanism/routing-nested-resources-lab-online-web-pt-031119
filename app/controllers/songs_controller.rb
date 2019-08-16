5ab9d4192818dcc5ae650e137bf2bc4ad8a2adac
Savannah C <sgcorrell@gmail.com>
2019-08-16 16:48:15 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,8 +1,10 @@
 class SongsController < ApplicationController
   def index
     if params[:artist_id]
-      @songs = Song.find(params[:artist_id])
+      @songs = Song.find(params[:artist_id]).songs
+    else
     @songs = Song.all
+    end
   end
 
   def show
